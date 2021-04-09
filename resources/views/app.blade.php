50217fb1561c1c1a9c16ed1015cc219bd5740ead
Lucas Leal <lucas.ity.leal@gmail.com>
2021-04-09 19:36:31 -0300

    Criação do menu principal

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -11,9 +11,23 @@
 </head>
 
 <body>
+    <nav class="navbar navbar-expand-lg navbar-dark bg-secondary">
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+    </button>
+        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+            <div class="navbar-nav">
+                <a class="nav-item nav-link" href="{{ route('products.list') }}">Produtos</a>
+            </div>
+        </div>
+    </nav>
+    <br>
     <main role="main" class="container">
         @yield('content')
     </main>
+
+    <script src="{{ url('js/jquery-3.2.1.slim.min.js') }}"></script>
+    <script src="{{ url('js/bootstrap.min.js') }}"></script>
 </body>
 
 </html>

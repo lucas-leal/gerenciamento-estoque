8a5f8323af4576eef63ca09ce3479019c73bebc3
Lucas Leal <lucas.ity.leal@gmail.com>
2021-04-10 11:49:52 -0300

    Deixando as rotas privadas.

--- routes/web.php
+++ routes/web.php
@@ -10,20 +10,22 @@ Route::get('login', [AuthController::class, 'index']);
 Route::post('login', [AuthController::class, 'login'])->name('login');
 Route::get('logout', [AuthController::class, 'logout'])->name('logout');
 
-Route::redirect('', 'products');
+Route::middleware('auth')->group(function () {
+    Route::redirect('', 'products');
 
-Route::get('products', [ProductController::class, 'list'])->name('products.list');
-Route::get('products/create', [ProductController::class, 'create'])->name('products.create');
-Route::post('products', [ProductController::class, 'store'])->name('products.store');
-Route::get('products/{id}/edit', [ProductController::class, 'edit'])->name('products.edit');
-Route::put('products/{id}', [ProductController::class, 'update'])->name('products.update');
-Route::get('products/{id}/delete', [ProductController::class, 'delete'])->name('products.delete');
-Route::delete('products/{id}', [ProductController::class, 'destroy'])->name('products.destroy');
+    Route::get('products', [ProductController::class, 'list'])->name('products.list');
+    Route::get('products/create', [ProductController::class, 'create'])->name('products.create');
+    Route::post('products', [ProductController::class, 'store'])->name('products.store');
+    Route::get('products/{id}/edit', [ProductController::class, 'edit'])->name('products.edit');
+    Route::put('products/{id}', [ProductController::class, 'update'])->name('products.update');
+    Route::get('products/{id}/delete', [ProductController::class, 'delete'])->name('products.delete');
+    Route::delete('products/{id}', [ProductController::class, 'destroy'])->name('products.destroy');
 
-Route::get('stock/add', [StockController::class, 'addForm'])->name('stock.add.form');
-Route::post('stock', [StockController::class, 'add'])->name('stock.add');
+    Route::get('stock/add', [StockController::class, 'addForm'])->name('stock.add.form');
+    Route::post('stock', [StockController::class, 'add'])->name('stock.add');
 
-Route::get('stock/remove', [StockController::class, 'removeForm'])->name('stock.remove.form');
-Route::patch('stock', [StockController::class, 'remove'])->name('stock.remove');
+    Route::get('stock/remove', [StockController::class, 'removeForm'])->name('stock.remove.form');
+    Route::patch('stock', [StockController::class, 'remove'])->name('stock.remove');
 
-Route::get('report', [ReportController::class, 'report'])->name('report');
+    Route::get('report', [ReportController::class, 'report'])->name('report');
+});

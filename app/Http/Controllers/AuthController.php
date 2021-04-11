650e866c8540a6ca152aaa494089be95fcd91e2a
Lucas Leal <lucas.ity.leal@gmail.com>
2021-04-10 21:33:56 -0300

    Geração de token para a API.

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -2,8 +2,11 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\User;
+use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Hash;
 
 class AuthController extends Controller
 {
@@ -28,5 +31,27 @@ class AuthController extends Controller
         Auth::logout();
 
         return redirect('/login');
+    }
+
+    public function apiLogin(Request $request)
+    {
+        $user = User::where('email', $request->email)->first();
+
+        if (!$user) {
+            return $this->getUnauthorizedResponse();
+        }
+        
+        if (!Hash::check($request->password, $user->password)) {
+            return $this->getUnauthorizedResponse();
+        }
+        
+        $token = $user->createToken('token');
+
+        return ['token' => $token->plainTextToken];
+    }
+
+    private function getUnauthorizedResponse(): JsonResponse
+    {
+        return response()->json(['message' => 'Credenciais inválidas'], 401);
     }
 }
